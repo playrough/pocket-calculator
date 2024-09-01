7ab74df723746003dc20d6f82ba5a204a461a8c5
playrough <106418248+playrough@users.noreply.github.com>
2024-09-01 12:29:58 +0700

    Update MayTinh.js

--- src/MayTinh/MayTinh.js
+++ src/MayTinh/MayTinh.js
@@ -58,6 +58,27 @@ export default class MayTinh extends Component {
 		this.setState({ result: value });
 	};
 
+	calculatorButtons = [
+		{ class: 'btnAC btnDark', action: () => this.Clear() },
+		{ class: 'btnDEL btnDark', action: () => this.Delete() },
+		{ class: 'btnDivi btnDark', action: () => this.AppendValue('/'), label: <i className="bi bi-slash-lg"></i> },
+		{ class: 'btn7', action: () => this.AppendValue(7), label: 7 },
+		{ class: 'btn8', action: () => this.AppendValue(8), label: 8 },
+		{ class: 'btn9', action: () => this.AppendValue(9), label: 9 },
+		{ class: 'btnMult btnDark', action: () => this.AppendValue('*'), label: <i className="bi bi-x-lg"></i> },
+		{ class: 'btn4', action: () => this.AppendValue(4), label: 4 },
+		{ class: 'btn5', action: () => this.AppendValue(5), label: 5 },
+		{ class: 'btn6', action: () => this.AppendValue(6), label: 6 },
+		{ class: 'btnSub btnDark', action: () => this.AppendValue('-'), label: <i className="bi bi-dash-lg"></i> },
+		{ class: 'btn1', action: () => this.AppendValue(1), label: 1 },
+		{ class: 'btn2', action: () => this.AppendValue(2), label: 2 },
+		{ class: 'btn3', action: () => this.AppendValue(3), label: 3 },
+		{ class: 'btnAdd btnDark', action: () => this.AppendValue('+'), label: <i className="bi bi-plus-lg"></i> },
+		{ class: 'btn0', action: () => this.AppendValue(0), label: 1 },
+		{ class: 'btnDot', action: () => this.AppendValue('.'), label: '.' },
+		{ class: 'btnEqual', action: () => this.Compute(), label: <i className="bi bi-pause"></i> },
+	];
+
 	render() {
 		return (
 			<div className="calculator">
@@ -65,60 +86,12 @@ export default class MayTinh extends Component {
 				<div className="display">
 					<div id="CurResult">{this.state.result || 0}</div>
 				</div>
-				<div className="button btnAC btnDark" onClick={() => this.Clear()}>
-					AC
-				</div>
-				<div className="button btnDEL btnDark" onClick={() => this.Delete()}>
-					DEL
-				</div>
-				<div className="button btnDivi btnDark" onClick={() => this.AppendValue('/')}>
-					<i className="bi bi-slash-lg"></i>
-				</div>
-				<div className="button btn7" onClick={() => this.AppendValue(7)}>
-					7
-				</div>
-				<div className="button btn8" onClick={() => this.AppendValue(8)}>
-					8
-				</div>
-				<div className="button btn9" onClick={() => this.AppendValue(9)}>
-					9
-				</div>
-				<div className="button btnMult btnDark" onClick={() => this.AppendValue('*')}>
-					<i className="bi bi-x-lg"></i>
-				</div>
-				<div className="button btn4" onClick={() => this.AppendValue(4)}>
-					4
-				</div>
-				<div className="button btn5" onClick={() => this.AppendValue(5)}>
-					5
-				</div>
-				<div className="button btn6" onClick={() => this.AppendValue(6)}>
-					6
-				</div>
-				<div className="button btnSub btnDark" onClick={() => this.AppendValue('-')}>
-					<i className="bi bi-dash-lg"></i>
-				</div>
-				<div className="button btn1" onClick={() => this.AppendValue(1)}>
-					1
-				</div>
-				<div className="button btn2" onClick={() => this.AppendValue(2)}>
-					2
-				</div>
-				<div className="button btn3" onClick={() => this.AppendValue(3)}>
-					3
-				</div>
-				<div className="button btnAdd btnDark" onClick={() => this.AppendValue('+')}>
-					<i className="bi bi-plus-lg"></i>
-				</div>
-				<div className="button btn0" onClick={() => this.AppendValue(0)}>
-					0
-				</div>
-				<div className="button btnDot" onClick={() => this.AppendValue('.')}>
-					.
-				</div>
-				<div className="button btnEqual" onClick={() => this.Compute()}>
-					<i className="bi bi-pause"></i>
-				</div>
+
+				{calculatorButtons.map(button => (
+					<div key={index} className={`button ${button.class}`} onClick={button.action}>
+						{button.label}
+					</div>
+				))}
 			</div>
 		);
 	}
